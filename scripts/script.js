b0bbbfa48e846351a184a7f59defd062986a3302
DamianR04 <damian.romeijn@hva.nl>
2024-01-18 20:20:04 +0100

    update v.06

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,29 @@
 
-    let downloadButton = document.querySelector("#downloadbtn");
+    let downloadButton = document.querySelector(".downloadfirst");
+    let downloadCloseButton1 = document.querySelector(".closebutton");
     let downloadInformation = document.querySelector(".information-download-container");
+    let textInformation = document.querySelector(".information-text")
 
     downloadButton.addEventListener("click", function () {
-        downloadInformation.classList.toggle("open");
+        downloadInformation.style.display = 'block';
+        downloadCloseButton1.style.display = 'block';
+        textInformation.style.display = 'none';
+        downloadButton.style.display= 'none';
     });
+    
 
-
-        let downloadCloseButton = document.querySelector("#closedownloadbtn");
+    
+    
+        let downloadFirstButton = document.querySelector(".downloadfirst");
+        let downloadCloseButton = document.querySelector(".closebutton");
         let downloadCloseInformation = document.querySelector(".information-download-container");
+        let textOpenInformation = document.querySelector(".information-text")
     
-        downloadCloseButton.addEventListener("click", function () {
-            downloadCloseInformation.classList.toggle("close");
+        downloadCloseButton.addEventListener("click", function (event) {
+            downloadCloseInformation.style.display = 'none';
+            downloadCloseButton.style.display = 'none';
+            textOpenInformation.style.display = 'block';
+            downloadFirstButton.style.display = 'block';
         });
+       
     
-      
